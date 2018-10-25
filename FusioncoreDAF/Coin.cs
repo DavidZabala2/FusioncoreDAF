165eaa7d7c51f583fd4489c74e39b2c14764a36b
elev <elev@5CG62366CS>
2018-10-25 13:10:22 +0200

    bakgrunden
    och kommands och nya filer har lagt tilll

---
+++ FusioncoreDAF/Coin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FusioncoreDAF
+{
+    class Coin : PhysicalObject
+    {
+        double timeToDie;
+
+        public Coin(Texture2D texture, float X, float Y, GameTime gameTime)
+            : base(texture, X, Y, 0, 2f)
+        {
+            timeToDie = gameTime.TotalGameTime.TotalMilliseconds + 5000;
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (timeToDie < gameTime.TotalGameTime.TotalMilliseconds)
+                isAlive = false;
+        }
+    }
+}
---
+++ FusioncoreDAF/Player.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace FusioncoreDAF
+{
+    class Player : PhysicalObject
+    {
+        Texture2D bulletTexture;
+        List<Trail> trail;
+        double timeSinceLastBullet = 0;
+        int points = 0;
+        public int Points { get { return points; } set { points = value; } }
+   
+        public  Player(Texture2D texture, float X, float Y, float speedX, float speedY, Texture2D bulletTexture) : base(texture, X, Y, speedX, speedY)
+        {
+        }
+
+        public void Reset(float X, float Y, float speedX, float speedY)
+        {
+            vector.X = X;
+            vector.Y = Y;
+            speed.X = speedX;
+            speed.Y = speedY;
+        }
+        public void Update(GameWindow window, GameTime gameTime)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (vector.X <= window.ClientBounds.Width - texture.Width && vector.X >= 0)
+            {
+                if (keyboardState.IsKeyDown(Keys.Right))
+                    vector.X += speed.X;
+                if (keyboardState.IsKeyDown(Keys.Left))
+                    vector.X -= speed.X;
+            }
+            if (vector.Y <= window.ClientBounds.Height - texture.Height && vector.Y >= 0)
+            {
+                if (keyboardState.IsKeyDown(Keys.Down))
+                    vector.Y += speed.Y;
+                if (keyboardState.IsKeyDown(Keys.Up))
+                    vector.Y -= speed.Y;
+            }
+
+            if (vector.X < 0)
+                vector.X = 0;
+
+            if (vector.X > window.ClientBounds.Width - texture.Width)
+            {
+                vector.X = window.ClientBounds.Width - texture.Width;
+            }
+            if (vector.Y < 0)
+                vector.Y = 0;
+            if (vector.Y > window.ClientBounds.Height - texture.Height)
+            {
+                vector.Y = window.ClientBounds.Height - texture.Height;
+            }
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
